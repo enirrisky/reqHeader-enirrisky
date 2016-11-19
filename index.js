9e42ae3ad02eac0248c76e7be9ea8539e5835445
Jonathan <Enirrisky@gmail.com>
2016-11-18 20:38:06 -0800

    test deploy

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ var app = express();
 
 app.use(express.static('public'));
 
+var port = process.env.PORT || 8080;
+
+app.listen(port, function(){
+  console.log("Listening on port: " + port);
+});
+
 app.get('/', function (req, res) {
    res.send('Hello World');
 })
 
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
-})
 
